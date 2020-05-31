4100c93c74826f3bf13ca5697b3836723361771e
Jonathan Chow <jonathan.chow@mclaren.com>
2020-05-31 21:58:08 +0100

    Removed comment out code

--- IPWebCam2KafkaConsumer.py
+++ IPWebCam2KafkaConsumer.py
@@ -1,30 +1,18 @@
 
-from urllib.request import urlopen
-import json
-from datetime import datetime
-from datetime import timedelta
-# import matplotlib.animation as animation
-# from matplotlib import style
 from kafka import KafkaConsumer
-from json import dumps
-import time
 import sys
 
 def start_consumer():
-    # topic = "test_1_12052020"
+    # Construct kafka consumer object
     consumer = KafkaConsumer(topic, bootstrap_servers=[kafkaserver])
-
     for msg in consumer:
+        # pring out value in msg, could have printed out the whole message as well
         print (msg.value)
 
 if __name__ == '__main__':
-    # argv[1] = host IP Adddress
-    # argv[2] = Kafka server IP Address and Port
-    # argv[3] = topic
-    # argv[4] = Phone IP Address
 
-    kafkaserver = sys.argv[1]
-    topic = sys.argv[2]
+    kafkaserver = sys.argv[1]   # Kafka server IP Address and Port ie 192.168.1.52:9092
+    topic = sys.argv[2]         # Kafka topic ie kafka_test
 
     start_consumer()
 
